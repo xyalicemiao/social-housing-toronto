37e9f33e8fe549da88a907af1edf3759281907bb
Xiaoyu Miao <xy.miao@mail.utoronto.ca>
2024-04-22 17:53:05 -0400

    commit

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,24 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
+# Purpose: Tests 
+# Author: Xiaoyu Miao
 # Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Contact: xy.miao@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Have the data downloaded
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+
+# Load data
+data <- read.csv(here::here("data/analysis_data.csv"))
+
+# Test to ensure all values are greater than zero
+test_results <- data %>% 
+  summarise(across(everything(), ~ all(. > 0)))
+
+# Print the test results
+print(test_results)
+
